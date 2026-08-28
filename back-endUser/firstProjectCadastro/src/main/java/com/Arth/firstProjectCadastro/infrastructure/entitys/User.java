23166304f7ff07946e31d23d1fd1586e41a4952d
Arthur Will <arthurwillyan0404fcl@gmail.com>
2026-08-28 19:01:56 -0300

    ajustes feitos na recuperação por email

--- back-endUser/firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/entitys/User.java
+++ back-endUser/firstProjectCadastro/src/main/java/com/Arth/firstProjectCadastro/infrastructure/entitys/User.java
@@ -1,8 +1,17 @@
 package com.Arth.firstProjectCadastro.infrastructure.entitys;
 
 
-import jakarta.persistence.*;
-import lombok.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Table(name = "Usuario")
 @Entity()
@@ -27,5 +36,5 @@ public class User {
     @Column(name = "senha")
     private String senha;
 
-    private Integer codigoRecuperacao;
+    private Integer CodigoRecuperacao;
 }
